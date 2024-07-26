c26a291d55f660036ea119f7f695ddbdc3e5cf2e
hanmilo <deanjywong@gmail.com>
2024-07-26 16:34:10 +0800

    Added input prompt & enabled basic calculations

--- script.js
+++ script.js
@@ -16,22 +16,49 @@ function divide(valueA, valueB) {
 };
 
 // functions in background
-function appendBody() {
+function appendMiddle() {
     // console.log("appendBody");
-    queryBody.appendChild(buttonStart);
-    queryBody.appendChild(buttonClear);
+    queryMiddle.appendChild(buttonStart);
+    queryMiddle.appendChild(buttonClear);
 };
 
 // main
 const queryMiddle = document.querySelector(".middle");
-queryMiddle.className = "queryMiddle";
+
+let input = 0;
+let arrayCalculate = [];
+var valueA = 0;
+var valueB = 0;
+var answer = 0;
+const arrayOperator = ["+", "-", "*", "/"];
+const arrayFunctionOperator = [add, subtract, multiply, divide];
+// const arrayFunctionOperator = [add(valueA, valueB), subtract(valueA, valueB), multiply(valueA, valueB), divide(valueA, valueB)];
 
 const buttonStart = document.createElement("button");
 buttonStart.className = "buttonStart";
 buttonStart.textContent = "Start";
+buttonStart.addEventListener("click", () => {
+    input = prompt("calculate: ", "0 + 0");
+    arrayCalculate = input.split(" ");
+    valueA = arrayCalculate[0];
+    operator = arrayCalculate[1];
+    valueB = arrayCalculate[2];
+    // valueA = Number(prompt("1st Number", 0));
+    // operator = prompt("Operator", "+");
+    // valueB = Number(prompt("2nd Number", 0));
+
+    for (let i = 0; i < arrayOperator.length; i++) {
+        if (operator === arrayOperator[i]) {
+            answer = arrayFunctionOperator[i](valueA, valueB);
+            alert(valueA + " " + operator + " " + valueB + " = " + answer);
+        };
+    }
+});
 
 const buttonClear = document.createElement("button");
 buttonClear.className = "buttonClear";
 buttonClear.textContent = "Clear";
+
+appendMiddle();
 
 
