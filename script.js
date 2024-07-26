818d5566c28f2ba2016b6b5438ccf4bb8efd4c4a
hanmilo <deanjywong@gmail.com>
2024-07-26 14:16:21 +0800

    Updated functions in script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// functions in calculator
 function add(valueA, valueB) {
     return valueA + valueB;
 };
@@ -13,4 +14,24 @@ function multiply(valueA, valueB) {
 function divide(valueA, valueB) {
     return valueA / valueB;
 };
+
+// functions in background
+function appendBody() {
+    // console.log("appendBody");
+    queryBody.appendChild(buttonStart);
+    queryBody.appendChild(buttonClear);
+};
+
+// main
+const queryMiddle = document.querySelector(".middle");
+queryMiddle.className = "queryMiddle";
+
+const buttonStart = document.createElement("button");
+buttonStart.className = "buttonStart";
+buttonStart.textContent = "Start";
+
+const buttonClear = document.createElement("button");
+buttonClear.className = "buttonClear";
+buttonClear.textContent = "Clear";
+
 
