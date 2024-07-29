cde374d997410db1888e7eac044e937fc1503d52
hanmilo <deanjywong@gmail.com>
2024-07-29 18:22:16 +0800

    Updated calculator display

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 // functions in calculator
+// function add(arrayValues) {
+//     return arrayValues.reduce((result, currentValue) => result + currentValue, 0);
+// };
+
+// function subtract(arrayValues) {
+//     return arrayValues.reduce((result, currentValue) => result - currentValue, 0);
+// };
+
+// function multiply(arrayValues) {
+//     return arrayValues.reduce((result, currentValue) => result * currentValue, 0);
+// };
+
+// function divide(arrayValues) {
+//     return arrayValues.reduce((result, currentValue) => result / currentValue, 0);
+// };
+
 function add(valueA, valueB) {
     return valueA + valueB;
 };
@@ -15,12 +31,19 @@ function divide(valueA, valueB) {
     return valueA / valueB;
 };
 
-function calculate(valueA, valueB, operator) {
-    for (let i = 1; i < arrayOperator.length; i++) {
-        if (operator == arrayOperator[i]) {
-            return arrayFunctionOperator[i](valueA, valueB);
-        }
-    }
+function calculate(arrayValues, arrayOperatorInput) {
+    var answer = 0;
+    let arrayValuesHolder = arrayValues;
+
+    for (let indexOperatorInput = 0; indexOperatorInput < arrayOperatorInput.length; indexOperatorInput++) {
+        for (let i = 1; i < arrayOperator.length; i++) {
+            if (arrayOperatorInput[indexOperatorInput] == arrayOperator[i]) {
+                answer = arrayFunctionOperator[i](arrayValuesHolder[0], arrayValuesHolder[1]);
+                arrayValues.splice(0, 1, answer);
+            };
+        };
+    };
+    return answer;
 };
 
 // functions in background
@@ -33,8 +56,11 @@ function clearAll() {
     arrayInput = [];
     indexCalculate = 0;
     arrayCalculate = [];
+    indexShow = 0;
+    arrayShow = [];
     answer = 0;
-    operator = "+";
+    arrayOperatorInput = [];
+    indexOperatorInput = 0;
 }
 
 // main
@@ -59,23 +85,33 @@ let indexInput = 0;
 let arrayInput = [];
 let indexCalculate = 0;
 let arrayCalculate = [];
+let indexShow = 0;
+let arrayShow = [];
+let arrayOperatorInput = [];
+let indexOperatorInput = 0;
 const arrayOperator = ["=", "+", "-", "*", "/"];
 const arrayFunctionOperator = [calculate, add, subtract, multiply, divide];
 const arrayCalculatorKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "C"];
 const arrayCalculatorPad = arrayCalculatorKeys.concat(arrayOperator);
 var answer = 0;
-var operator = "+";
 // console.log(arrayCalculator);
+
+const displayInput = document.createElement("h2");
+displayInput.classList = "displayInput";
+// displayInput.textContent = arrayInput;
+// displayRow1.appendChild(displayInput);
 
 for (let i = 0; i < arrayCalculatorPad.length; i++) {
     const buttonPad = document.createElement("button");
-    buttonPad.className = "buttonPad";
+    buttonPad.classList = "buttonPad";
     buttonPad.textContent = arrayCalculatorPad[i];
     buttonPad.addEventListener("click", () => {
         if (i < 10) {   // Number button pressed
             arrayInput[indexInput] = arrayCalculatorPad[i];
-            console.log(arrayInput);
+            arrayShow[indexShow] = arrayInput[indexInput];
+
             indexInput++;
+            indexShow++;
         }
         else if (i == 10) { // "clear" button pressed
             clearAll();
@@ -83,20 +119,30 @@ for (let i = 0; i < arrayCalculatorPad.length; i++) {
         else if (i == 11) { // "=" button pressed
             arrayCalculate[indexCalculate] = Number(arrayInput.join(""));
             // indexCalculate++;
-            answer = calculate(arrayCalculate[0], arrayCalculate[1], operator);
-            alert(arrayCalculate[0] + " " + operator + " " + arrayCalculate[1] + " = " + answer);
-            clearAll();
+            answer = calculate(arrayCalculate, arrayOperatorInput);
+            arrayShow[indexShow] = "=";
+            indexShow++;
+            arrayShow[indexShow] = answer;
+            // alert(arrayCalculate[0] + " " + operator + " " + arrayCalculate[1] + " = " + answer);
+            // clearAll();
         }
         else {  // operator button pressed
             arrayCalculate[indexCalculate] = Number(arrayInput.join(""));
             indexCalculate++;
-            console.log(arrayCalculate);
 
-            operator = arrayCalculatorPad[i];
+            arrayOperatorInput[indexOperatorInput] = arrayCalculatorPad[i];
+
+            arrayShow[indexShow] = arrayOperatorInput[indexOperatorInput];
+            indexOperatorInput++;
+            indexShow++;
 
             arrayInput = [];
             indexInput = 0;
         };
+        console.log(arrayShow);
+
+        displayInput.textContent = arrayShow.join("");
+        displayRow1.appendChild(displayInput);
     });
 
     if (i < 4) {
