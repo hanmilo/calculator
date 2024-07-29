d7f0cc4a6a291119e28b4158737bfdefebf3644f
hanmilo <deanjywong@gmail.com>
2024-07-29 14:56:35 +0800

    Updated with working calculator

--- script.js
+++ script.js
@@ -15,50 +15,85 @@ function divide(valueA, valueB) {
     return valueA / valueB;
 };
 
-// functions in background
-function appendMiddle() {
-    // console.log("appendBody");
-    queryMiddle.appendChild(buttonStart);
-    // queryMiddle.appendChild(buttonClear);
+function calculate(valueA, valueB, operator) {
+    for (let i = 1; i < arrayOperator.length; i++) {
+        if (operator == arrayOperator[i]) {
+            return arrayFunctionOperator[i](valueA, valueB);
+        }
+    }
 };
+
+// functions in background
+function appendMiddle(appendItem) {
+    queryMiddle.appendChild(appendItem);
+};
+
+function clearAll() {
+    indexInput = 0;
+    arrayInput = [];
+    indexCalculate = 0;
+    arrayCalculate = [];
+    answer = 0;
+    operator = "+";
+}
 
 // main
 const queryMiddle = document.querySelector(".middle");
 
-let input = 0;
+let indexInput = 0;
+let arrayInput = [];
+let indexCalculate = 0;
 let arrayCalculate = [];
-var valueA = 0;
-var valueB = 0;
+const arrayOperator = ["=", "+", "-", "*", "/"];
+const arrayFunctionOperator = [calculate, add, subtract, multiply, divide];
+const arrayCalculatorKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "clear"];
+const arrayCalculatorPad = arrayCalculatorKeys.concat(arrayOperator);
 var answer = 0;
-const arrayOperator = ["+", "-", "*", "/"];
-const arrayFunctionOperator = [add, subtract, multiply, divide];
-// const arrayFunctionOperator = [add(valueA, valueB), subtract(valueA, valueB), multiply(valueA, valueB), divide(valueA, valueB)];
+var operator = "+";
+// console.log(arrayCalculator);
 
-const buttonStart = document.createElement("button");
-buttonStart.className = "buttonStart";
-buttonStart.textContent = "Start";
-buttonStart.addEventListener("click", () => {
-    input = prompt("calculate: ", "0 + 0");
-    arrayCalculate = input.split(" ");
-    valueA = arrayCalculate[0];
-    operator = arrayCalculate[1];
-    valueB = arrayCalculate[2];
-    // valueA = Number(prompt("1st Number", 0));
-    // operator = prompt("Operator", "+");
-    // valueB = Number(prompt("2nd Number", 0));
+for (let i = 0; i < arrayCalculatorPad.length; i++) {
+    const buttonPad = document.createElement("button");
+    buttonPad.className = "buttonPad";
+    buttonPad.textContent = arrayCalculatorPad[i];
+    buttonPad.addEventListener("click", () => {
+        // input = prompt("calculate: ", "0 + 0");
+        // arrayCalculate = input.split(" ");
+        // valueA = arrayCalculate[0];
+        // operator = arrayCalculate[1];
+        // valueB = arrayCalculate[2];
+        // valueA = Number(prompt("1st Number", 0));
+        // operator = prompt("Operator", "+");
+        // valueB = Number(prompt("2nd Number", 0));
 
-    for (let i = 0; i < arrayOperator.length; i++) {
-        if (operator === arrayOperator[i]) {
-            answer = arrayFunctionOperator[i](valueA, valueB);
-            alert(valueA + " " + operator + " " + valueB + " = " + answer);
+        // console.log(typeof Number(arrayCalculatorPad));
+        if (i < 10) {   // Number button pressed
+            arrayInput[indexInput] = arrayCalculatorPad[i];
+            console.log(arrayInput);
+            indexInput++;
+        }
+        else if (i == 10) { // "clear" button pressed
+            clearAll();
+        }
+        else if (i == 11) { // "=" button pressed
+            arrayCalculate[indexCalculate] = Number(arrayInput.join(""));
+            // indexCalculate++;
+            answer = calculate(arrayCalculate[0], arrayCalculate[1], operator);
+                alert(arrayCalculate[0] + " " + operator + " " + arrayCalculate[1] + " = " + answer);
+            clearAll();
+        }
+        else {  // operator button pressed
+            arrayCalculate[indexCalculate] = Number(arrayInput.join(""));
+            indexCalculate++;
+            console.log(arrayCalculate);
+
+            operator = arrayCalculatorPad[i];
+
+            arrayInput = [];
+            indexInput = 0;
         };
-    }
-});
-
-// const buttonClear = document.createElement("button");
-// buttonClear.className = "buttonClear";
-// buttonClear.textContent = "Clear";
-
-appendMiddle();
+    });
+    appendMiddle(buttonPad);
+};
 
 
